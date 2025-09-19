4b3b20629842fcf365d4a3f8ad256c2d7f60c3d6
Spita Alexandru <spita.alexandrum@gmail.com>
2025-09-19 23:55:17 +0300

    Adding react to my project

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,16 @@ int main()
     crow::mustache::set_base(currDir.string());
 
     CROW_ROUTE(app, "/").methods(crow::HTTPMethod::GET)([]() {
-    	auto page = crow::mustache::load("mainpage.html");
-    	
+    	auto page = crow::mustache::load("index.html");    	
     	return page.render();
     });
 
+    CROW_ROUTE(app, "/static/<string>")
+    .methods(crow::HTTPMethod::GET)([](std::string filename)
+    {
+    	std :: string completePath = "/src/" + filename;
+    	auto response = crow::mustache::load(completePath);
+    	return response.render();
+    });    
     app.bindaddr("127.0.0.1").port(18080).multithreaded().run();
 }
