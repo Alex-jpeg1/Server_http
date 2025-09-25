26fb516fe27999b2cc2289dcde525a8811bae4f3
Spita Alexandru <spita.alexandrum@gmail.com>
2025-09-25 23:37:50 +0300

    Managing routes

--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,51 @@ class Server{
 	ip(addr)
 	{}
 
+	void setupRoutes()
+	{
+		auto currDir = fs::current_path();
+		crow::mustache::set_base(currDir.string());
+		
+		app.route_dynamic("/").methods(crow::HTTPMethod::GET)([]())
+		{
+			auto page =crow::mustache::load("index.html");
+			return page.render(); 
+		}};
+
+		app.route_dynamic("/static/<string>").methods(crow::HTTPMethod::GET)([](std::string filename)
+		{
+			std :: string completePaht = currDir.string() + "templates" + filename;
+			auto response = crow::mustache::load(completePath);
+			return response.render();
+		});
+
+		app.route_dynamic("/login").methods(crow::HTTPMethod::POST)([](const crow::request req)
+		{
+			auto body = crow::query_string(req.body);
+
+			std::string user = body.get("user") ? body.get("user") : "";
+			std::strign pass = body.get("pass") ? body.get("pass") : "";
+
+			// Check if the user is trying to indentify correctly
+
+			
+			if(user.size() ==  0)
+			{
+				return crow::response(401, "Username must be an array");
+			}
+			if(pass.size() == 0)
+			{
+				return crow::response(401, "Password cannot be empty");
+			}
+			
+		});
+	}
+	
 	void run ()
 	{
 		app.bindaddr(ip).port(Port).multithreaded().run();			
 	}
+
 	
  private:
 	int Port;
